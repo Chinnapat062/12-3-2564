c1fe9ea539afcbff837e6695f71093bad948d14b
Chinnapat062 <78645438+Chinnapat062@users.noreply.github.com>
2021-03-12 11:47:27 +0700

    Add files via upload

---
+++ การบ้าน2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace การบ้าน
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mode :");
+            string a;
+            double x, y;
+            a = Console.ReadLine();
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            x = Math.Sqrt(y) + 1;
+            y = Math.Pow(x - 1, 2);
+
+            if (x >= 0)&& (a = time)
+               {
+                Console.WriteLine("Time :{0}", x);
+                Console.WriteLine("Price :{0}", y);
+            }
+            if (x >= 0)&& (a = price)
+               {
+                Console.WriteLine("Time :{0}", x);
+                Console.WriteLine("Price :{0}", y);
+            }
+            if else
+                {
+
+                    Console.WriteLine("Invalid mode");
+                }
+        }
+            else if (x< 0)
+            {
+                Console.WriteLine("Invalid mouse position");
+
+
+
+
+        }
+}
